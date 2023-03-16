8ce97a83b1cb73a6fd59c18404c7e3518f8c47de
digitradiart <digitradiart@gmail.com>
2023-03-16 09:43:28 +0700

    navbar script

---
+++ script/navbar-scroll.js
@@ -0,0 +1,7 @@
+//jquery
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
